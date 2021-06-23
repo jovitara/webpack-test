bd74dedb7185db3aca5798f2a2489a2c8b09ad46
jovitara <83599193+jovitara@users.noreply.github.com>
2021-06-23 20:32:37 +0300

    06-23 nd

--- src/js/app.js
+++ src/js/app.js
@@ -10,13 +10,10 @@ import 'slick-carousel';
 
 // alert('labas');
 
-$(document).ready(function() {
-  $('.autoplay').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
 });
 
 // alert("labas");
